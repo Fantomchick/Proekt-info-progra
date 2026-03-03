c20df03efec6b926b53599d2cf2fe0357d8ab1fb
unknown <iskkab2000@yanndex.ru>
2026-03-03 22:42:44 +0500

    1

--- proect_info_progra_app/proect_info_progra_app/models.py
+++ proect_info_progra_app/proect_info_progra_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from transliterate import translit
+# from transliterate import translit
 
 class Topic(models.Model):
     #CharField(max_length = 25)
